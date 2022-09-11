651c52edfaf7497c9739e6f5fe1c8e1aac5040cc
Atzcck <alptazecicek@gmail.com>
2022-09-11 23:50:47 +0300

    comments edited.

--- app/src/main/java/com/alptazecicek/savingstate/SavingStateFragment.kt
+++ app/src/main/java/com/alptazecicek/savingstate/SavingStateFragment.kt
@@ -19,19 +19,19 @@ class SavingStateFragment : Fragment() {
     ): View? {
         Log.v("PATIKA", "onCreateView called")
         // Inflate the layout for this fragment
-
         fragmentSavingStateBinding = FragmentSavingStateBinding.inflate(inflater)
         return fragmentSavingStateBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        //onView creation if it has a null bundle value. TextView's text will be 'enter a name'
+        //which is it's null at the beginning. With second creations it will have bundle value.
         if (savedInstanceState?.getString("textString") == null)
             fragmentSavingStateBinding.textView.text = "Enter a Name"
         else fragmentSavingStateBinding.textView.text = savedInstanceState
             .getString("textString").toString()
-
+        //onClick listenner added to save button
         fragmentSavingStateBinding.apply {
             save.setOnClickListener {
                 textView.text = editTextTextPersonName.text.toString()
@@ -41,6 +41,7 @@ class SavingStateFragment : Fragment() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        //Bundle is creating in Save instance state and giving a key string.
         outState.putString("textString", fragmentSavingStateBinding.textView.text.toString())
     }
 }
